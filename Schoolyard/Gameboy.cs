8e145e3a69685c252aad209348bbfd376614bdaa
Corwin McKnight <lioneltabre@gmail.com>
2018-03-27 12:47:44 -0700

    PPU Mode switching logic

--- Schoolyard/Gameboy.cs
+++ Schoolyard/Gameboy.cs
@@ -21,30 +21,28 @@ namespace Schoolyard
             loader = new ROMLoader(this);
             memory = new MemoryController();
             cpu = new LR35902(this);
-            ppu = new PPU();
+            ppu = new PPU(memory);
         }
 
         public void SetupMemoryMap()
         {
-            // 0x0000 - 0xA000 ROM
-
-            RAM bgData = new RAM("bg", 0x9800, 0x800);              // BG Map 1 - 2
-            RAM wram   = new RAM("wram", 0xC000, 0x2000);           // Work Ram
-            RAM hwio   = new RAM("hwio - unmapped", 0xFF00, 0x80);  // Unmapped HWIO (so it doesn't crash)
-            RAM hiram  = new RAM("hiram", 0xFF80, 0x7F);            // Zero page ram
-            RAM ie     = new RAM("ie", 0xFFFF, 0x01);               // Interrupt enable bte
+            RAM bgData = new RAM("bg", 0x9800, 0x800);
+            RAM wram   = new RAM("wram", 0xC000, 0x2000);
+            RAM hwio   = new RAM("hwio - unmapped", 0xFF00, 0x80);
+            RAM hiram  = new RAM("hiram", 0xFF80, 0x7F);
+            RAM ie     = new RAM("ie", 0xFFFF, 0x01);
 
             // Memory Mapping rules
             // 1. Order determines priorities
             // 2. Multiple devices can be mapped to the same address
-
-            memory.Map(ppu.cram); // 0x8000 - 0x97FF
-            memory.Map(bgData);   // 0x9800 - 0x9FFF
-            memory.Map(wram);     // 0xC000 - 0xDFFF
-            memory.Map(ppu.regs); // 0xFF40
-            memory.Map(hwio);     // 0xFF00 - 0xFF80
-            memory.Map(hiram);    // 0xFF80 - 0xFFFE
-            memory.Map(ie);       // 0xFFFF
+            //                       0x0000 - 0xA000 ROM
+            memory.Map(ppu.cram); // 0x8000 - 0x97FF CRAM
+            memory.Map(bgData);   // 0x9800 - 0x9FFF BG 1 and 2
+            memory.Map(wram);     // 0xC000 - 0xDFFF Work RAM
+            memory.Map(ppu.regs); // 0xFF40 - 0xFF47 PPU Registers
+            memory.Map(hwio);     // 0xFF00 - 0xFF80 Unmapped HWIO
+            memory.Map(hiram);    // 0xFF80 - 0xFFFE Zero Page
+            memory.Map(ie);       // 0xFFFF          Interrupt enable byte
         }
 
         public void Reset()
--- Schoolyard/LCD/PPU.cs
+++ Schoolyard/LCD/PPU.cs
@@ -3,31 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Schoolyard.Memory;
 namespace Schoolyard.LCD
 {
     public class PPU
     {
-        public PPURegisters regs;
-        public PPUCharacterRAM cram;
-
-        public const int width = 160;
-        public const int height = 144;
+        public enum PPUMode
+        {
+            H_BLANK = 0,
+            V_BLANK = 1,
+            OAM_READ = 2,
+            VRAM_READ = 3,
+        }
 
         // Holds full image
         public byte[,] framebuffer = new byte[width, height];
         public int framesRendered = 0;
 
+        public PPUMode currentMode = PPUMode.H_BLANK;
         public ulong modeClock = 0;
 
-        public PPU()
+        public PPURegisters regs;
+        public PPUCharacterRAM cram;
+        public MemoryController mem;
+
+        // Constants
+        public const int width = 160;
+        public const int height = 144;
+        public const ulong clocksHBlank = 204;
+        public const ulong clocksVBlank = 456;
+        public const ulong clocksOAMRead = 80;
+        public const ulong clocksVRAMRead = 172;
+
+        public PPU(MemoryController mem)
         {
             regs = new PPURegisters("ppuregs", 0xFF40, 0x9);
             cram = new PPUCharacterRAM("cram", 0x8000, 0x1800);
+            this.mem = mem;
         }
 
         public void Reset() {
+            currentMode = PPUMode.H_BLANK;
             modeClock = 0;
+
             framebuffer = new byte[width, height]; // Clear framebuffer
             framesRendered = 0;
         }
@@ -35,9 +53,83 @@ namespace Schoolyard.LCD
         public void Step(ulong cycles)
         {
             // Advance PPU by x cycles
+            switch (currentMode)
+            {
+                case PPUMode.H_BLANK:
+                    ModeHBlank();
+                    break;
+                case PPUMode.V_BLANK:
+                    ModeVBlank();
+                    break;
+                case PPUMode.OAM_READ:
+                    ModeOAMRead();
+                    break;
+                case PPUMode.VRAM_READ:
+                    ModeVRAMRead();
+                    break;
+            }
         }
 
-        public void DrawScanline()
+        private void ModeHBlank()
+        {
+            if (modeClock >= clocksHBlank)
+            {
+                modeClock -= clocksHBlank;
+                regs.ScanLine++;
+                if (regs.ScanLine >= height - 1)
+                {
+                    byte flags = mem.Read8(0xFF0F);
+                    byte enable = mem.Read8(0xFFFF);
+
+                    if ((enable & 0x1) != 0)
+                    {
+                        flags |= 0x1;
+                        mem.Write8(0xFF0F, flags);
+                    }
+
+                    currentMode = PPUMode.V_BLANK;
+                    OnRenderComplete();
+                    return;
+                }
+
+                currentMode = PPUMode.OAM_READ;
+            }
+        }
+
+        private void ModeVBlank()
+        {
+            if (modeClock >= clocksVBlank)
+            {
+                modeClock -= clocksVBlank;
+                regs.ScanLine++;
+                if (regs.ScanLine >= 153)
+                {
+                    regs.ScanLine = 0;
+                    currentMode = PPUMode.H_BLANK;
+                }
+            }
+        }
+
+        private void ModeOAMRead()
+        {
+            if (modeClock >= clocksOAMRead)
+            {
+                modeClock -= clocksOAMRead;
+                currentMode = PPUMode.VRAM_READ;
+            }
+        }
+
+        private void ModeVRAMRead()
+        {
+            if (modeClock >= clocksVRAMRead)
+            {
+                modeClock -= clocksVRAMRead;
+                currentMode = PPUMode.H_BLANK;
+                DrawScanline();
+            }
+        }
+
+        private void DrawScanline()
         {
             if(regs.ScanLine >= height) { // Should never hit this, but here to be safe
                 return; 
@@ -53,12 +145,17 @@ namespace Schoolyard.LCD
             }
         }
 
-        public void DrawBackgroundScanLine()
+        private void DrawBackgroundScanLine()
         {
 
         }
 
-        public void DrawSpriteScanLine()
+        private void DrawSpriteScanLine()
+        {
+
+        }
+
+        private void OnRenderComplete()
         {
 
         }
