42ff9edc47882bdf0f5eaabdf519b68611d7bbe0
Corwin McKnight <lioneltabre@gmail.com>
2018-03-28 13:25:08 -0700

    Better Timer implementation

--- Schoolyard/CPU/Timer.cs
+++ Schoolyard/CPU/Timer.cs
@@ -10,34 +10,48 @@ namespace Schoolyard.CPU
     {
         private Memory.MemoryController mem;
         public const int BaseFreqency = 16384;
-        private ulong currentCycles = 0;
-        private ulong internalCounter = 0;
-        private ulong internalCounterDiv = 0;
+
+        private ushort internalCounter = 0;
+        private byte timer = 0;
 
         // Timer Modulo
         public byte DIV
         {
-            get { return Read8(0xFF04); }
-            set { values[0] = value; } 
+            // DIV is the higher 8 bits of the internal counter
+            get { return (byte)((internalCounter >> 8) & 0xFF); }
+            set { internalCounter = 0; } 
         }
 
         // Timer Counter
-        public byte TIMA
+        public int TIMA
         {
-            get { return Read8(0xFF05); }
-            set { Write8(0xFF05, value); }
+            get {
+                return timer;
+            }
+            set {
+                if(value > 0xFF)
+                {
+                    // Raise interrupt
+                    timer = TMA; // Reset
+                }
+                else
+                {
+                    timer = (byte)value;
+                }
+            }
         }
 
         // Timer Modulo
-        public byte TMA {
-            get { return Read8(0xFF06); }
-            set { Write8(0xFF06, value); }
-        }
+        public byte TMA { get; set; }
 
         public byte TAC
         {
-            get { return Read8(0xFF07); }
-            set { Write8(0xFF07, value); }
+            get; set;
+        }
+
+        public bool TimerEnabled
+        {
+            get { return ((Read8(0xFF07) >> 2) & 1) == 1; }
         }
 
         public int TACCycles
@@ -59,6 +73,30 @@ namespace Schoolyard.CPU
             }
         }
 
+        public int InternalCounter
+        {
+            get
+            {
+                return internalCounter;
+            }
+
+            set
+            {
+                int temp = internalCounter;
+                internalCounter = (ushort)value;
+                int temp2 = 0;
+
+                int cycles = TACCycles;
+
+                if(TimerEnabled)
+                {
+                    temp &= ~(TACCycles - 1);
+                    temp2 = (internalCounter - temp) / cycles;
+                    TIMA += (byte)temp2;
+                }
+            }
+        }
+
         public Timer(Memory.MemoryController m) : base("timer", 0xFF04, 4)
         {
             mem = m;
@@ -66,40 +104,48 @@ namespace Schoolyard.CPU
 
         public override void Write8(ushort address, byte val)
         {
-            if(address == 0xFF04)
+            switch (address)
             {
-                val = 0;
+                case 0xFF04:
+                    DIV = 0;
+                    break;
+                case 0xFF05:
+                    TIMA = 0; // TODO: Verify if this behavior is correct
+                    break;
+                case 0xFF06:
+                    TMA = val;
+                    break;
+                case 0xFF07:
+                    TAC = val;
+                    break;
+                default:
+                    int translatedAddress = address - addressBase;
+                    values[translatedAddress] = val;
+                    break;
             }
-
-            int translatedAddress = address - addressBase;
-            values[translatedAddress] = val;
         }
 
         public void Step(ulong cyclesDelta)
         {
-            int tima = TIMA;
-            internalCounter += cyclesDelta;
-
-            // Increment tima according to TACC Cycles
-
-            if (tima > 0xFF)
-            {
-                tima = TMA;
-                // Request interrupt here
-            }
-
-            // Increment DIV now
-            internalCounterDiv += cyclesDelta;
-            if (internalCounterDiv > 16)
-            {
-                internalCounterDiv -= 16;
-                DIV++;
-            }
+            internalCounter += (ushort)cyclesDelta;
         }
 
         public override byte Read8(ushort address)
         {
-            return base.Read8(address);
+            switch (address)
+            {
+                case 0xFF04:
+                    return DIV;
+                case 0xFF05:
+                    return (byte)TIMA;
+                case 0xFF06:
+                    return TMA;
+                case 0xFF07:
+                    return TAC;
+                default:
+                    int translatedAddress = address - addressBase;
+                    return values[translatedAddress];
+            }
         }
     }
 }
