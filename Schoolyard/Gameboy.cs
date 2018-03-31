272b83aae9bb8bfee2a0060b760f73d21cea3f9b
Corwin McKnight <lioneltabre@gmail.com>
2018-03-31 14:57:58 -0700

    Keypad input code

--- Schoolyard/Gameboy.cs
+++ Schoolyard/Gameboy.cs
@@ -17,6 +17,7 @@ namespace Schoolyard
         public MemoryController memory;
         public Timer timer;
         public DMAController dma;
+        public Input.Keypad keypad;
         public Gameboy()
         {
             loader = new ROMLoader(this);
@@ -25,6 +26,7 @@ namespace Schoolyard
             ppu = new PPU(this);
             timer = new Timer(this);
             dma = new DMAController(this);
+            keypad = new Input.Keypad();
         }
 
         public void SetupMemoryMap()
@@ -41,6 +43,7 @@ namespace Schoolyard
             // 2. Multiple devices can be mapped to the same address
             //                        0x0000 - 0xA000 ROM
             memory.Map(dma);
+            memory.Map(hiram);     // 0xFF80 - 0xFFFE Zero Page
             memory.Map(intf);
             memory.Map(ppu.cram);  // 0x8000 - 0x97FF CRAM
             memory.Map(ppu.bgram); // 0x9800 - 0x9FFF BG 1 and 2
@@ -50,9 +53,41 @@ namespace Schoolyard
             memory.Map(r);         // 0xFEA0 - 0xFEFF Unmapped
             memory.Map(timer);
             memory.Map(ppu.regs);  // 0xFF40 - 0xFF47 PPU Registers
+            memory.Map(keypad);    // 0xFF00
             memory.Map(hwio);      // 0xFF00 - 0xFF80 Unmapped HWIO
-            memory.Map(hiram);     // 0xFF80 - 0xFFFE Zero Page
             memory.Map(inte);        // 0xFFFF          Interrupt enable byte
+
+            memory.Write8(0xFF05, 0x00);
+            memory.Write8(0xFF06, 0x00);
+            memory.Write8(0xFF07, 0x00);
+            memory.Write8(0xFF10, 0x80);
+            memory.Write8(0xFF11, 0xBF);
+            memory.Write8(0xFF12, 0xF3);
+            memory.Write8(0xFF14, 0xBF);
+            memory.Write8(0xFF16, 0x3F);
+            memory.Write8(0xFF17, 0x00);
+            memory.Write8(0xFF19, 0xBF);
+            memory.Write8(0xFF1A, 0x7F);
+            memory.Write8(0xFF1B, 0xFF);
+            memory.Write8(0xFF1C, 0x9F);
+            memory.Write8(0xFF1E, 0xBF);
+            memory.Write8(0xFF20, 0xFF);
+            memory.Write8(0xFF21, 0x00);
+            memory.Write8(0xFF22, 0x00);
+            memory.Write8(0xFF23, 0xBF);
+            memory.Write8(0xFF24, 0x77);
+            memory.Write8(0xFF25, 0xF3);
+            memory.Write8(0xFF26, 0xF1);
+            memory.Write8(0xFF40, 0x91);
+            memory.Write8(0xFF42, 0x00);
+            memory.Write8(0xFF43, 0x00);
+            memory.Write8(0xFF45, 0x00);
+            memory.Write8(0xFF47, 0xFC);
+            memory.Write8(0xFF48, 0xFF);
+            memory.Write8(0xFF49, 0xFF);
+            memory.Write8(0xFF4A, 0x00);
+            memory.Write8(0xFF4B, 0x00);
+            memory.Write8(0xFFFF, 0x00);
         }
 
         public void Reset()
--- Schoolyard/Input/Keypad.cs
+++ Schoolyard/Input/Keypad.cs
@@ -8,26 +8,97 @@ namespace Schoolyard.Input
 {
     public class Keypad : Memory.MemoryDevice
     {
-        private byte ioPort = 0x00;
+        public int bank = 0;
+        public byte[] values = new byte[2];
 
-        public override ushort Read16(ushort address)
+        // Bank 0
+        public bool Start {
+            set {
+                Set(0, maskStart, value);
+            }
+        }
+        public bool Select {
+            set {
+                Set(0, maskSelect, value);
+            }
+        }
+        public bool A {
+            set {
+                Set(0, maskA, value);
+            }
+        }
+        public bool B {
+            set {
+                Set(0, maskB, value);
+            }
+        }
+
+        // Bank 1
+        public bool Up {
+            set {
+                Set(1, maskUp, value);
+            }
+        }
+        public bool Down {
+            set {
+                Set(1, maskDown, value);
+            }
+        }
+        public bool Left {
+            set {
+                Set(1, maskLeft, value);
+            }
+        }
+        public bool Right {
+            set {
+                Set(1, maskRight, value);
+            }
+        }
+
+        // Bank 0
+        private const byte maskA = 0x1;
+        private const byte maskB = 0x2;
+        private const byte maskSelect = 0x4;
+        private const byte maskStart = 0x8;
+
+        // Bank 1
+        private const byte maskRight = 0x1;
+        private const byte maskLeft = 0x2;
+        private const byte maskUp = 0x4;
+        private const byte maskDown = 0x8;
+
+        public Keypad()
         {
-            throw new NotImplementedException();
+            name = "keypad";
+            addressBase = 0xFF00;
+            size = 1;
         }
 
         public override byte Read8(ushort address)
         {
-            throw new NotImplementedException();
-        }
-
-        public override void Write16(ushort address, ushort val)
-        {
-            throw new NotImplementedException();
+            return values[bank];
         }
 
         public override void Write8(ushort address, byte val)
         {
-            throw new NotImplementedException();
+            if ((val & 0x10) != 0) {
+                bank = 0;
+            }
+            else if ((val & 0x20) != 0) {
+                bank = 1;
+            }
+        }
+
+        public void Set(int bank, byte mask, bool pressed)
+        {
+            if(pressed)
+            {
+                values[bank] = (byte)(values[bank] | mask);
+            }
+            else
+            {
+                values[bank] = (byte)(values[bank] & (~mask));
+            }
         }
     }
 }
